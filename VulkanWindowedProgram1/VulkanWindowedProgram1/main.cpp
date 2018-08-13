d3220668c9e1493b5c0fe72fd721811d4426cf19
Spenser Flitton <spenser.flitton@us.ophiropt.com>
2018-08-13 16:13:09 -0600

    Getting physical device and queue families

--- VulkanWindowedProgram1/VulkanWindowedProgram1/main.cpp
+++ VulkanWindowedProgram1/VulkanWindowedProgram1/main.cpp
@@ -73,6 +73,16 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 	}
 }
 
+struct QueueFamilyIndices
+{
+	int graphicsFamily = -1;
+
+	bool isComplete()
+	{
+		return graphicsFamily >= 0;
+	}
+};
+
 class VulkanTriangleApplication
 {
 public:
@@ -94,6 +104,7 @@ private:
 	SDL_Window * window;
 	VkInstance instance;
 	VkDebugUtilsMessengerEXT callback;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
 	bool initWindow()
 	{
@@ -246,6 +257,76 @@ private:
 		}
 	}
 
+	bool isDeviceSuitable(VkPhysicalDevice device)
+	{
+		/*VkPhysicalDeviceProperties deviceProperties;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+		
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;*/
+		QueueFamilyIndices indices = findQueueFamilies(device);
+
+		return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+	{
+		QueueFamilyIndices indices;
+
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		int index = 0;
+		for (const auto& queueFamily : queueFamilies)
+		{
+			if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.graphicsFamily = index;
+			}
+
+			if (indices.isComplete())
+			{
+				break;
+			}
+
+			index++;
+		}
+
+		return indices;
+	}
+
+	void pickPhysicalDevice()
+	{
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+		if (deviceCount == 0)
+		{
+			throw std::runtime_error("failed to find GPUs with Vulkan support!");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for (const auto& device : devices)
+		{
+			if (isDeviceSuitable(device))
+			{
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE)
+		{
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+	}
+
 	bool initVulkan()
 	{
 		if (!createInstance())
@@ -253,6 +334,7 @@ private:
 			return false;
 		}
 		setupDebugCallback();
+		pickPhysicalDevice();
 
 		return true;
 	}
